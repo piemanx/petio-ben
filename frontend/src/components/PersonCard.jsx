b052b75f77e2ed8196bc4fd2b29c283e341ee39c
YourGitHubUsername <your.email@example.com>
2026-02-24 17:56:19 -0500

    Refactor PersonCard.jsx to functional component

--- frontend/src/components/PersonCard.jsx
+++ frontend/src/components/PersonCard.jsx
@@ -1,75 +1,63 @@
-import React from "react";
-import { withRouter, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
-class PersonCard extends React.Component {
-  constructor(props) {
-    super(props);
+const PersonCard = (props) => {
+  const [imgLoaded, setImgLoaded] = useState(false);
 
-    this.state = {
-      imgLoaded: false,
-    };
+  const onImgLoad = () => {
+    setImgLoaded(true);
+  };
 
-    this.imgLoaded = this.imgLoaded.bind(this);
+  let knownFor = null;
+  let person = props.person;
+  if (!person || person.gender === 0) {
+    return null;
   }
-
-  imgLoaded() {
-    this.setState({
-      imgLoaded: true,
+  if (person.known_for && person.known_for.length) {
+    person.known_for.map((item) => {
+      return (
+        <Link key={`${item.id}__kf`} to={`/movie/${item.id}`}>
+          {item.title}
+        </Link>
+      );
     });
   }
 
-  render() {
-    let knownFor = null;
-    let person = this.props.person;
-    if (!person || person.gender === 0) {
-      return null;
-    }
-    if (person.known_for && person.known_for.length) {
-      person.known_for.map((item) => {
-        return (
-          <Link key={`${item.id}__kf`} to={`/movie/${item.id}`}>
-            {item.title}
-          </Link>
-        );
-      });
-    }
-
-    return (
-      <div
-        key={person.id}
-        className={`card person-card ${
-          this.state.imgLoaded ? "img-loaded" : "img-not-loaded"
-        }`}
-      >
-        <div className="card--inner">
-          <Link to={`/person/${person.id}`} className="full-link"></Link>
-          <div className="image-wrap">
-            <LazyLoadImage
-              alt={person.name}
-              src={
-                person.profile_path
-                  ? `https://image.tmdb.org/t/p/w200${person.profile_path}`
-                  : `${window.location.pathname.replace(
-                      /\/$/,
-                      ""
-                    )}/images/no-poster-person.jpg`
-              }
-              onLoad={this.imgLoaded}
-            />
-          </div>
-          <div className="text-wrap">
-            <p className="title">{person.name}</p>
-            {knownFor ? <p className="known-for">{knownFor}</p> : null}
-            {this.props.character ? (
-              <p className="character">{this.props.character}</p>
-            ) : null}
-          </div>
+  return (
+    <div
+      key={person.id}
+      className={`card person-card ${
+        imgLoaded ? "img-loaded" : "img-not-loaded"
+      }`}
+    >
+      <div className="card--inner">
+        <Link to={`/person/${person.id}`} className="full-link"></Link>
+        <div className="image-wrap">
+          <LazyLoadImage
+            alt={person.name}
+            src={
+              person.profile_path
+                ? `https://image.tmdb.org/t/p/w200${person.profile_path}`
+                : `${window.location.pathname.replace(
+                    /\/$/,
+                    ""
+                  )}/images/no-poster-person.jpg`
+            }
+            onLoad={onImgLoad}
+          />
+        </div>
+        <div className="text-wrap">
+          <p className="title">{person.name}</p>
+          {knownFor ? <p className="known-for">{knownFor}</p> : null}
+          {props.character ? (
+            <p className="character">{props.character}</p>
+          ) : null}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default withRouter(PersonCard);
+export default PersonCard;
